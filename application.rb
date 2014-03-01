e9b38de0ab5b3f9db4b57423d02bb5ba3c381124
Brian Pattison <brian@brianpattison.com>
2014-03-01 17:24:07 -0600

    Make the secret token an env variable

--- application.rb
+++ application.rb
@@ -568,6 +568,26 @@ end
 PRODUCTION
 
 # -----------------------------------------
+# config/initializers/secret_token.rb
+# -----------------------------------------
+
+run "rm config/initializers/secret_token.rb"
+file "config/initializers/secret_token.rb", <<-SECRETTOKEN
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+#{@app_name.underscore.classify}::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
+SECRETTOKEN
+
+# -----------------------------------------
 # config/initializers/smtp.rb
 # -----------------------------------------
 file "config/initializers/smtp.rb", <<-SMTP
@@ -1094,6 +1114,8 @@ NORMALIZECSS
 domain_name = ask("What is the host name used in the production environment?")
 file ".env", <<-ENV
 HOSTNAME=localhost:3000
+
+SECRET_TOKEN=#{SecureRandom.hex(64)}
 
 SMTP_ADDRESS=smtp.mandrillapp.com
 SMTP_PORT=587
