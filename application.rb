4b37723508c6b3812dd759e8414d86da40d90f1c
Brian Pattison <brian@brianpattison.com>
2014-03-05 15:07:52 -0600

    Fix body class

--- application.rb
+++ application.rb
@@ -418,11 +418,9 @@ module ApplicationHelper
   include LocalTimeHelper
   
   def body_class
-    @body_class ||= "default"
-    body_class = "blogelator"
+    body_class = @body_class || "default"
     body_class += " " + controller.controller_name.dasherize
     body_class += " " + controller.action_name.dasherize
-    body_class += " " + @body_class
     body_class
   end
   
