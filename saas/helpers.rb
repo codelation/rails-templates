7177d310ecade3f37ccf7912ff6620a329e5a03e
Brian Pattison <brian@brianpattison.com>
2014-12-15 14:33:13 -0600

    Fix ApplicationHelper indentation

--- saas/helpers.rb
+++ saas/helpers.rb
@@ -3,59 +3,59 @@ module Saas
 
     def self.application(app_name)
       return <<-APPLICATION
-  module ApplicationHelper
-    include LocalTimeHelper
+module ApplicationHelper
+  include LocalTimeHelper
 
-    def body_class
-      body_class = @body_class || "default"
-      body_class += " " + controller.controller_name.dasherize
-      body_class += " " + controller.action_name.dasherize
-      body_class
+  def body_class
+    body_class = @body_class || "default"
+    body_class += " " + controller.controller_name.dasherize
+    body_class += " " + controller.action_name.dasherize
+    body_class
+  end
+
+  def page_title
+    page = @title
+    if page
+      "\#{page} | \#{site_title}"
+    else
+      site_title
     end
+  end
 
-    def page_title
-      page = @title
-      if page
-        "\#{page} | \#{site_title}"
-      else
-        site_title
-      end
-    end
+  def site_title
+    "#{app_name.titleize}"
+  end
 
-    def site_title
-      "#{app_name.titleize}"
-    end
-
-    def subscriber_params
-      if @organization && @organization.persisted?
+  def subscriber_params
+    if @organization && @organization.persisted?
+      {
+        resource_name: "organizations",
+        subscriber_id: @organization.id
+      }
+    elsif current_user && current_user.active_subscription?
+      {
+        resource_name: "users",
+        subscriber_id: current_user.id
+      }
+    elsif current_user
+      if organization = current_user.organizations.find{|organization| organization.active_subscription? }
         {
           resource_name: "organizations",
-          subscriber_id: @organization.id
+          subscriber_id: organization.id
         }
-      elsif current_user && current_user.active_subscription?
+      else
         {
           resource_name: "users",
           subscriber_id: current_user.id
         }
-      elsif current_user
-        if organization = current_user.organizations.find{|organization| organization.active_subscription? }
-          {
-            resource_name: "organizations",
-            subscriber_id: organization.id
-          }
-        else
-          {
-            resource_name: "users",
-            subscriber_id: current_user.id
-          }
-        end
-      else
-        {}
       end
+    else
+      {}
     end
-
   end
-  APPLICATION
+
+end
+APPLICATION
     end
 
     def self.subscription_plans
